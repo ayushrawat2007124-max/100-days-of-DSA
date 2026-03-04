cf43b0762f1e39d99e3b223749cc774a3dd4c105
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-03-04 22:35:12 +0530

    Add files via upload

---
+++ 20.c
@@ -0,0 +1,66 @@
+/*Problem: Given an array of integers, count the number of subarrays whose sum is equal to zero.
+
+Input:
+- First line: integer n
+- Second line: n integers
+
+Output:
+- Print the count of subarrays having sum zero
+
+Example:
+Input:
+6
+1 -1 2 -2 3 -3
+
+Output:
+6
+
+Explanation: A subarray is a continuous part of the array. For the input array 1 -1 2 -2 3 -3,
+ the following subarrays have sum zero: [1, -1], [2, -2], [3, -3], [1, -1, 2, -2], [2, -2, 3, -3], and [1, -1, 2, -2, 3, -3]. 
+ Since there are 6 such subarrays, the output is 6.*/
+ 
+ #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100000
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    long long count = 0;
+    long long prefixSum = 0;
+
+    
+    int size = 2 * MAX + 1;
+    long long *freq = (long long*)calloc(size, sizeof(long long));
+
+    
+    int offset = MAX;
+
+    for(int i = 0; i < n; i++) {
+        prefixSum += arr[i];
+
+        if(prefixSum == 0) {
+            count++;
+        }
+
+    
+        if(freq[prefixSum + offset] > 0) {
+            count += freq[prefixSum + offset];
+        }
+
+       
+        freq[prefixSum + offset]++;
+    }
+
+    printf("%lld\n", count);
+
+    free(freq);
+    return 0;
+}
